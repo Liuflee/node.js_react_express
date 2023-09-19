041dd5655395244d6817f25d7355c0d53293f6c0
Anette Villalón <liuflee@outlook.com>
2023-09-18 23:09:23 -0300

    gato

--- practica1/src/index.js
+++ practica1/src/index.js
@@ -1,85 +1,120 @@
 import React, { useState, useEffect } from 'react';
-import ReactDOM from 'react-dom/client';
-import empanaImage from './images/catempana.jpg';
-import gifImage from './images/happy-happy-happy-cat.gif';
-import audioHappy from './audio/happysong.mp3';
+import ReactDOM from 'react-dom';
 
+/**
+ * Creacion de los assets que se utilizaran en la aplicacion
+ */
+const assets = {
+  empanaImage: require('./images/catempana.jpg'),
+  happyGIF: require('./images/happy-happy-happy-cat.gif'),
+  audioHappy: require('./audio/happysong.mp3'),
+  sadGIF: require('./images/sadcat.gif'),
+  audioSad: require('./audio/sadsong.mp3'),
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const App = () => {
   const [counter, setCounter] = useState(0);
   const [showImage, setShowImage] = useState(false);
-  const [showGift, setShowGift] = useState(false);
+  const [isHappy, setIsHappy] = useState(false);
+  const [isSad, setIsSad] = useState(false);
+  const [showButton, setShowButton] = useState(false);
 
-  const cambiarImagen = () => {
+  /**
+   * Se utiliza un useEffect para que cada segundo se incremente el contador
+   * y se muestre el mensaje correspondiente
+   * Si el contador llega a 10, se muestra la imagen de la empanada
+   */
+
+  useEffect(() => {
+    if (showImage) { // Si ya se muestra la imagen, no se hace nada
+      return;
+    }
+
+    const timer = setTimeout(() => { // Se crea un timer para que se incremente el contador cada segundo
+      setCounter(prevCounter => prevCounter + 1);
+    }, 1000);
+
+    if (counter === 10) {
+      setShowImage(true);
+      setShowButton(true);
+    }
+
+    return () => { // Se limpia el timer 
+      clearTimeout(timer);
+    };
+  }, [counter, showImage]);
+
+  /**
+   * Se crean dos funciones para manejar los clicks de los botones
+   * Si se da click en el boton de comer empanada, se muestra la imagen de gato feliz
+   * y se reproduce el audio correspondiente
+   * Si se da click en el boton de no dar empanada, se muestra la imagen de gato triste
+   * y se reproduce el audio correspondiente
+   */
+
+  const handleHappyClick = () => {
     setShowImage(false);
-    setShowGift(true);
-    const audio = new Audio(audioHappy);
+    setIsHappy(true);
+    const audio = new Audio(assets.audioHappy);
+    audio.loop = true;
     audio.play();
   };
 
-  useEffect(() => {
-    // Verifica si showImage es true, en cuyo caso no hacemos nada y retornamos
-    if (showImage) {
-      return;
-    }
-  
-    const timer = setTimeout(() => {
-
-      setCounter((prevCounter) => prevCounter + 1);
-    }, 1000);
-    
-    if (counter === 10) {
-      setShowImage(true);
-    }
-
-    return () => {
-      clearTimeout(timer);
-    };
-
-  }, [counter, showImage]);
-  
-
-  const rootStyle = {
-    backgroundColor: 'yellow',
-    textAlign: 'center',
+  const handleSadClick = () => {
+    setShowImage(false);
+    setIsSad(true);
+    const audio = new Audio(assets.audioSad);
+    audio.loop = true;
+    audio.play();
   };
 
-  const imageStyle = {
-    margin: '0 auto',
-    display: 'block',
-    padding: '20px',
-  };
+  /**
+   * Se muestra un mensaje dependiendo del estado de la aplicacion
+   * Si el gato esta triste, se muestra un mensaje de bad ending
+   * Si el gato esta feliz, se muestra un mensaje de good ending
+   * Si no es ninguna de las 2, se muestra un mensaje de espera
+   */
 
-  const buttonStyle = {
-    fontSize: '20px',
-    padding: '10px 20px',
-    display: 'block',
-    margin: '0 auto',
-  };
-  
   return (
-    <div style={rootStyle}>
-      {showGift ? (
+    <div style={{ backgroundColor: 'yellow', textAlign: 'center' }}>
+      {isSad ? (
+        <>
+          <h1>BAD ENDING: No comí empanada, moriré de hambre</h1>
+          <img src={assets.sadGIF} alt="Empanada" style={{ margin: '0 auto', display: 'block', padding: '20px' }} />
+        </>
+      ) : isHappy ? (
+
         <>
           <h1>Empanada comida, estoy feliz</h1>
-          <img src={gifImage} alt="Empanada" style={imageStyle} />
+          <img src={assets.happyGIF} alt="Empanada" style={{ margin: '0 auto', display: 'block', padding: '20px' }} />
         </>
+
       ) : showImage ? (
+
         <>
           <h1>Ya pasaron más de 10 segundos, necesito comer una empanada</h1>
-          <img src={empanaImage} alt="Empanada" style={imageStyle} />
-          <button onClick={cambiarImagen} style={buttonStyle}>
+          <img src={assets.empanaImage} alt="Empanada" style={{ margin: '0 auto', display: 'block', padding: '20px' }} />
+          <button onClick={handleHappyClick} style={{ fontSize: '20px', padding: '10px 20px', display: 'block', margin: '0 auto' }}>
             Comer empanada
           </button>
+          {showButton && (
+            <button onClick={handleSadClick} style={{ fontSize: '20px', padding: '10px 20px', display: 'block', margin: '0 auto' }}>
+              No dar empanada
+            </button>
+          )}
         </>
+
       ) : (
         <h1>Han pasado {counter} segundos desde que me comí una empanada</h1>
-        
       )}
     </div>
   );
 };
 
+/**
+ * Se renderiza la aplicacion en el elemento con id root
+ */
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
